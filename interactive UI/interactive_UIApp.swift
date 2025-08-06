8dc63ba27b99d50877ad8265c7e73018e817842b
Kylie Davis <kylie@jtde.com>
2025-08-06 14:52:43 -0700

    Initial Commit

---
+++ interactive UI/interactive_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactive_UIApp.swift
+//  interactive UI
+//
+//  Created by Scholar on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct interactive_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
