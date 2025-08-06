1cd4a70c8404b3a65ceac503c299ba798098c495
Kylie Davis <kylie@jtde.com>
2025-08-06 14:58:52 -0700

    added text

--- interactive UI/ContentView.swift
+++ interactive UI/ContentView.swift
@@ -9,14 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("What is your name?")
+                .font(.title)
+            
+            
+            
         }
-        .padding()
-    }
+        }
 }
 
 #Preview {
